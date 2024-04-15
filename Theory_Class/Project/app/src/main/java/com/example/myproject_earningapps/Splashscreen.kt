9802e964cd076991a954377b0ca620f1f6deec7a
Cuke <114121702+TranTrongSang@users.noreply.github.com>
2024-04-15 13:20:50 +0700

    Add: Splash Screen & Sign Up screen

--- Theory_Class/Project/app/src/main/java/com/example/myproject_earningapps/Splashscreen.kt
+++ Theory_Class/Project/app/src/main/java/com/example/myproject_earningapps/Splashscreen.kt
@@ -1,6 +1,9 @@
 package com.example.myproject_earningapps
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,22 @@ class Splashscreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splashscreen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+}
+
+private fun Handler.postDelayed(function: () -> Unit) {
+
 }
