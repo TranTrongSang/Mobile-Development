8c5754188967db1f928f8d4b78353d5ef48e5615
Cuke <114121702+TranTrongSang@users.noreply.github.com>
2024-04-15 14:44:43 +0700

    Alter: Android manifest & Add: Activity: Home

--- Theory_Class/Project/app/src/main/java/com/example/myproject_earningapps/Splashscreen.kt
+++ Theory_Class/Project/app/src/main/java/com/example/myproject_earningapps/Splashscreen.kt
@@ -23,10 +23,10 @@ class Splashscreen : AppCompatActivity() {
 
 
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
