6cd582500f29a8d251cb259cb1fbd25bce2c4f8f
Cuke <114121702+TranTrongSang@users.noreply.github.com>
2024-04-16 21:59:11 +0700

    Add: Fragments

--- Theory_Class/Project/app/src/main/java/com/example/myproject_earningapps/HomeActivity.kt
+++ Theory_Class/Project/app/src/main/java/com/example/myproject_earningapps/HomeActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +16,20 @@ class HomeActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
 
+        // Find the NavController using the ID of the NavHostFragment
+        val navController = findNavController(R.id.fragmentContainerView)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        // Find the BottomNavigationView
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        // Set up BottomNavigationView with NavController
+        bottomNav.setupWithNavController(navController)
+
+        // Optional: Handle window insets to adjust padding
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
